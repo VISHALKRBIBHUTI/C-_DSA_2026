784a53480a1f857999efc7bf27cd8213ac69a348
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-08-30 14:31:18 +0530

    Add inverted pattern

---
+++ Pattern_Question/invertedPat.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace  std;
+
+int main(){
+
+    int Intri = 4;
+
+    for(int i = 1 ; i<=Intri ; i++){
+
+        for(int j = 1 ; j<=(Intri + 1 - i); j++){
+
+            cout<<"*"<<" ";
+        }
+
+        cout<<'\n';
+    }
+
+    return 0;
+}
