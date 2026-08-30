ef04bba5373de9fb0a41d0fa27f7c3314532960d
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-08-30 14:32:14 +0530

    Add Star Pattern

---
+++ Pattern_Question/starPat.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    int tri = 4;
+
+    for(int i = 1; i <= tri ; i++){
+
+        for(int j = 1 ; j<=i ; j++){
+
+            cout<<"*"<<" ";
+        }
+
+        cout<<'\n';
+    }
+    
+
+    return 0;
+}
