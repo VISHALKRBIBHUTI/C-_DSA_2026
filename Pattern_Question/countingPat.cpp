7b8d9db9b8bba50c170690defa809321b6dc75a1
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-08-30 14:27:57 +0530

    Add counting pattern

---
+++ Pattern_Question/countingPat.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int row = 4;
+    int column = 4;
+
+    for(int i = 1 ; i<=row ; i++){
+
+        for(int j = 1 ; j<=column ; j++){
+
+            cout<<i<<" ";
+        }
+
+        cout<<'\n';
+    }
+
+    return 0;
+}
